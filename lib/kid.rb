3e6521abc2cd6d120f0800296230800a041817aa
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-08 16:07:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,15 +2,15 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-#class Kid 
+class Kid 
   
  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+include FancyDance::InstanceMethods
  
- # attr_accessor :name
+  attr_accessor :name
  
- # def initialize(name)
- #   @name = name
- # end
+  def initialize(name)
+    @name = name
+  end
   
-#end
+end
