7e0a086e74f2e76e9be3ec297b038d4704d023ea
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-08 16:06:53 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,19 +3,14 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend FancyDance::ClassMethods
+  
+ extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
 end
-
-#class Dancer
-  
- # include Dance
- # extend MetaDancing
- 
- # attr_accessor :name
- 
- # def initialize(name)
-  #  @name = name
-  #end
-  
-#end
